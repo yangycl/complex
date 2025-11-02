541f7993c82dcd06703ca138ded0050f467a6822
yangycl <yangyangyclee@gmail.com>
2025-11-02 09:14:06 +0800

    增加.d.ts

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare class Complex {
+    real: number;
+    imag: number;
+    sign: "+" | "-" | "";
+    
+    constructor(comStr: string);
+
+    static add(comarr: Complex[]): Complex;
+    static subtract(comarr: Complex[]): Complex;
+    static multiply(comarr: Complex[]): Complex;
+    static divide(comarr: Complex[]): Complex;
+
+    toString(): string;
+}
